8c579b54d7c8036d3178ae4f6b8d69ca982fe28a
Arevik Khachatryan <arevik.kh.y@gmail.com>
2025-06-22 22:28:09 +0400

    Update and rename to external_api_usage.py

--- external_api_usage.py
+++ external_api_usage.py
@@ -1,5 +1,6 @@
 import requests
 
+
 response = requests.post('http://127.0.0.1:5000/api/summarize', 
                        files={'file': open('C:/Users/user/Desktop/diploma codes/AMM.pdf', 'rb')})
 print(response.json())
